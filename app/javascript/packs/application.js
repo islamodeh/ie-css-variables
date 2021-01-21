b5c1e49ea0d1a611dd41a40d01ee37db16d09332
Islam Odeh <islamodeh@islams-macbook-pro.home>
2021-01-21 15:30:50 +0200

    add css vars package.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,11 +7,11 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+import cssVars from 'css-vars-ponyfill'
 
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+document.addEventListener("DOMContentLoaded", function(event) {
+	cssVars({
+		rootElement: document,
+		onlyLegacy: false
+	});
+});
